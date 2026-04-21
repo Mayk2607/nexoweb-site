c47143cfc9a7ee8eb5a60d6f7fcefb0b0f57a24d
Mario Murillo <mariomurillo@192.168.1.17>
2026-04-21 13:15:33 -0600

    fix: remove unused ts-expect-error directive to fix Vercel build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // @ts-expect-error: Next.js suggests this config but hasn't updated its typings for it yet.
   allowedDevOrigins: ['192.168.1.17', 'localhost', '0.0.0.0'],
 };
 
